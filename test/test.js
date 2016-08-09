9614282fd497ab823961dcf96efeb95930a93226
takayama.daisuke <takayama.daisuke@moneyforward.co.jp>
2016-08-09 19:40:19 +0900

    add test file

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var assert = require('power-assert');
+
+var FixedTable = require('../dist/fixed_table.js');
+
+describe('FixedTable', function() {
+  var fixedTable = new global.FixedTable();
+
+  it('Create Instance', function() {
+    assert(fixedTable instanceof Object);
+  });
+
+  describe('Public Function', function() {
+
+  });
+});
