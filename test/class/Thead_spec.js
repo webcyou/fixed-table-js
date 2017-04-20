285ed46950207d2f31efd7acb70f7c8a3fff9c86
webcyou <info@webcyou.com>
2017-04-20 16:44:00 +0900

    refactor Thead_spec

--- test/class/Thead_spec.js
+++ test/class/Thead_spec.js
@@ -1,12 +1,16 @@
 const assert = require('power-assert');
+const Table  = require('../../dist/fixed_table.js').FixedTable.Table;
 const Thead  = require('../../dist/fixed_table.js').FixedTable.Thead;
-const Cell = require('../../dist/fixed_table.js').FixedTable.Cell;
+const Cell   = require('../../dist/fixed_table.js').FixedTable.Cell;
 
 describe('Thead', function() {
-  var thead,
-      cells = [];
+  var table,
+      thead,
+      cells;
 
   beforeEach(function(done) {
+    cells = [];
+
     const THEAD_DATA = {
       lineNum: 0,
       width: 1233,
@@ -19,7 +23,13 @@ describe('Thead', function() {
       boxSizing: 'border-box'
     };
 
+    const TABLE_DATA = {
+      outerWidth: 1300,
+      borderLeftWidth: '4px'
+    };
+
     thead = Thead.fromData(THEAD_DATA);
+    table = Table.fromData(TABLE_DATA);
 
     for (var i = 0; i < 10; i++) {
       cells.push(Cell.fromData({
@@ -64,9 +74,51 @@ describe('Thead', function() {
   });
 
   describe('public Function setLineNumber', function() {
-    it('The value is set for lineNum.', function() {
+    it('The value is set for lineNum', function() {
       thead.setLineNumber(5);
       assert.equal(thead.lineNum, 5);
+    });
+  });
+
+  describe('public Function setCells', function() {
+    it('Set the cell array', function() {
+      thead.setCells(cells);
+      assert(thead.cells.length === 10);
+    });
+  });
+
+  describe('public Function getCells', function() {
+    it('Acquire the array of the corresponding cell', function() {
+      thead.setCells(cells);
+      assert(thead.getCells(1, 0).length === 1);
+    });
+  });
+
+  describe('public Function getCell', function() {
+    it('Acquire the corresponding cell', function() {
+      thead.setCells(cells);
+      assert(thead.getCell(1, 0).id === 1);
+    });
+  });
+
+  describe('public Function setStyles', function() {
+    it('Set table and set the width of thead', function() {
+      thead.setCells(cells);
+      thead.setStyles(table);
+
+      assert(thead.width === 1298);
+      assert(thead.outerWidth === 1250);
+    });
+  });
+
+  describe('public Function setSelfStyles', function() {
+    it('The width is set to thead', function() {
+      thead.setCells(cells);
+      thead.setSelfStyles(thead);
+
+      assert(thead.width === 983);
+      assert(thead.outerWidth === 1250);
+      assert(thead.outerHeight === 50);
     });
   });
 
